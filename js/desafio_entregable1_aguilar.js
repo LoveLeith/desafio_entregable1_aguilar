b5f8a18e8aacb5a7d0bd53b4d943c86bbbff66ae
Paula Aguilar <mariapaulaaguilar0@gmail.com>
2021-09-06 20:08:43 -0300

    pedido de info y cálculo edad en js

--- js/desafio_entregable1_aguilar.js
+++ js/desafio_entregable1_aguilar.js
@@ -1,5 +1,15 @@
-let nombreIngresado = prompt("Ingresa tu nombre");
+let nombreIngresado = prompt("Por favor, ingresa tu nombre");
 
 let resultado = "Hola," + " " + nombreIngresado;
 
 alert(resultado);
+
+let anioActual = prompt("¿En qué año estamos?");
+let fechaNacimiento = prompt("¿En qué año naciste?");
+
+let anio = parseInt(anioActual);
+let edad = parseInt(fechaNacimiento);
+
+let restaAnios = anioActual - fechaNacimiento;
+
+alert("Tu edad es:" + " " + restaAnios);
